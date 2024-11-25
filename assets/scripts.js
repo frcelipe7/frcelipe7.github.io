552aaff6665239f32710f843b75ae8522312520d
Felipe Raphael Coelho dos Santos <feliperaphaelpara@gmail.com>
2024-11-25 13:38:09 -0300

    novo modelo

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,89 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // adicionando os icones em tecnologias utilizadas na section de projetos
-    var tech_icons = document.querySelectorAll("#projects .project_line .icons .icon");
+    const lang_switch = document.querySelector("#lang_switch input");
+    const a_download_cv = document.querySelector("#inicio .right .curriculo");
 
-    let figma_svg_icon = `<svg id="figma_icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48"><path fill="#e64a19" d="M26,17h-8c-3.866,0-7-3.134-7-7v0c0-3.866,3.134-7,7-7h8V17z"></path><path fill="#7c4dff" d="M25,31h-7c-3.866,0-7-3.134-7-7v0c0-3.866,3.134-7,7-7h7V31z"></path><path fill="#66bb6a" d="M18,45L18,45c-3.866,0-7-3.134-7-7v0c0-3.866,3.134-7,7-7h7v7C25,41.866,21.866,45,18,45z"></path><path fill="#ff7043" d="M32,17h-7V3h7c3.866,0,7,3.134,7,7v0C39,13.866,35.866,17,32,17z"></path><circle cx="32" cy="24" r="7" fill="#29b6f6"></circle></svg>`;
-    let github_svg_icon = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 72 72"><path d="M36,12c13.255,0,24,10.745,24,24c0,10.656-6.948,19.685-16.559,22.818c0.003-0.009,0.007-0.022,0.007-0.022	s-1.62-0.759-1.586-2.114c0.038-1.491,0-4.971,0-6.248c0-2.193-1.388-3.747-1.388-3.747s10.884,0.122,10.884-11.491	c0-4.481-2.342-6.812-2.342-6.812s1.23-4.784-0.426-6.812c-1.856-0.2-5.18,1.774-6.6,2.697c0,0-2.25-0.922-5.991-0.922	c-3.742,0-5.991,0.922-5.991,0.922c-1.419-0.922-4.744-2.897-6.6-2.697c-1.656,2.029-0.426,6.812-0.426,6.812	s-2.342,2.332-2.342,6.812c0,11.613,10.884,11.491,10.884,11.491s-1.097,1.239-1.336,3.061c-0.76,0.258-1.877,0.576-2.78,0.576	c-2.362,0-4.159-2.296-4.817-3.358c-0.649-1.048-1.98-1.927-3.221-1.927c-0.817,0-1.216,0.409-1.216,0.876s1.146,0.793,1.902,1.659	c1.594,1.826,1.565,5.933,7.245,5.933c0.617,0,1.876-0.152,2.823-0.279c-0.006,1.293-0.007,2.657,0.013,3.454	c0.034,1.355-1.586,2.114-1.586,2.114s0.004,0.013,0.007,0.022C18.948,55.685,12,46.656,12,36C12,22.745,22.745,12,36,12z"></path></svg>`;
-    let python_svg_icon = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48"><path fill="#0277BD" d="M24.047,5c-1.555,0.005-2.633,0.142-3.936,0.367c-3.848,0.67-4.549,2.077-4.549,4.67V14h9v2H15.22h-4.35c-2.636,0-4.943,1.242-5.674,4.219c-0.826,3.417-0.863,5.557,0,9.125C5.851,32.005,7.294,34,9.931,34h3.632v-5.104c0-2.966,2.686-5.896,5.764-5.896h7.236c2.523,0,5-1.862,5-4.377v-8.586c0-2.439-1.759-4.263-4.218-4.672C27.406,5.359,25.589,4.994,24.047,5z M19.063,9c0.821,0,1.5,0.677,1.5,1.502c0,0.833-0.679,1.498-1.5,1.498c-0.837,0-1.5-0.664-1.5-1.498C17.563,9.68,18.226,9,19.063,9z"></path><path fill="#FFC107" d="M23.078,43c1.555-0.005,2.633-0.142,3.936-0.367c3.848-0.67,4.549-2.077,4.549-4.67V34h-9v-2h9.343h4.35c2.636,0,4.943-1.242,5.674-4.219c0.826-3.417,0.863-5.557,0-9.125C41.274,15.995,39.831,14,37.194,14h-3.632v5.104c0,2.966-2.686,5.896-5.764,5.896h-7.236c-2.523,0-5,1.862-5,4.377v8.586c0,2.439,1.759,4.263,4.218,4.672C19.719,42.641,21.536,43.006,23.078,43z M28.063,39c-0.821,0-1.5-0.677-1.5-1.502c0-0.833,0.679-1.498,1.5-1.498c0.837,0,1.5,0.664,1.5,1.498C29.563,38.32,28.899,39,28.063,39z"></path></svg>`;
-    let html_svg_icon = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48"><path fill="#E65100" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"></path><path fill="#FF6D00" d="M24 8L24 39.9 35.2 36.7 37.7 8z"></path><path fill="#FFF" d="M24,25v-4h8.6l-0.7,11.5L24,35.1v-4.2l4.1-1.4l0.3-4.5H24z M32.9,17l0.3-4H24v4H32.9z"></path><path fill="#EEE" d="M24,30.9v4.2l-7.9-2.6L15.7,27h4l0.2,2.5L24,30.9z M19.1,17H24v-4h-9.1l0.7,12H24v-4h-4.6L19.1,17z"></path></svg>`;
-    let css_svg_icon = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48"><path fill="#0277BD" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"></path><path fill="#039BE5" d="M24 8L24 39.9 35.2 36.7 37.7 8z"></path><path fill="#FFF" d="M33.1 13L24 13 24 17 28.9 17 28.6 21 24 21 24 25 28.4 25 28.1 29.5 24 30.9 24 35.1 31.9 32.5 32.6 21 32.6 21z"></path><path fill="#EEE" d="M24,13v4h-8.9l-0.3-4H24z M19.4,21l0.2,4H24v-4H19.4z M19.8,27h-4l0.3,5.5l7.9,2.6v-4.2l-4.1-1.4L19.8,27z"></path></svg>`;
-    let js_svg_icon = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48"><path fill="#ffd600" d="M6,42V6h36v36H6z"></path><path fill="#000001" d="M29.538 32.947c.692 1.124 1.444 2.201 3.037 2.201 1.338 0 2.04-.665 2.04-1.585 0-1.101-.726-1.492-2.198-2.133l-.807-.344c-2.329-.988-3.878-2.226-3.878-4.841 0-2.41 1.845-4.244 4.728-4.244 2.053 0 3.528.711 4.592 2.573l-2.514 1.607c-.553-.988-1.151-1.377-2.078-1.377-.946 0-1.545.597-1.545 1.377 0 .964.6 1.354 1.985 1.951l.807.344C36.452 29.645 38 30.839 38 33.523 38 36.415 35.716 38 32.65 38c-2.999 0-4.702-1.505-5.65-3.368L29.538 32.947zM17.952 33.029c.506.906 1.275 1.603 2.381 1.603 1.058 0 1.667-.418 1.667-2.043V22h3.333v11.101c0 3.367-1.953 4.899-4.805 4.899-2.577 0-4.437-1.746-5.195-3.368L17.952 33.029z"></path></svg>`;
-    let django_svg_icon = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256"><g fill="#2ba977" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8,8)"><path d="M21,4h4v4h-4zM25,22.754c0,1.35 -0.317,5.799 -4.242,7.149l-3.758,-1.903c2.806,-1.224 4,-3.2 4,-5.246c0,-2.828 0,-11.754 0,-11.754l4,0.002c0,0 0,8.713 0,11.752zM14,4v6.302c-0.469,-0.191 -1.48,-0.228 -1.978,-0.228c-3.749,0 -7.022,1.937 -7.022,6.926c0,5.969 4.504,6.997 7.65,6.997c1.242,0 4,-0.122 5.35,-0.513v-19.484zM12.609,20.666c-1.769,0 -3.609,-0.803 -3.609,-3.666c0,-2.863 1.84,-3.469 3.609,-3.469c0.432,0 0.918,0.081 1.391,0.189v6.757c-0.472,0.107 -0.958,0.189 -1.391,0.189z"></path></g></g></svg>`;
-    let git_svg_icon = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48"><path fill="#F4511E" d="M42.2,22.1L25.9,5.8C25.4,5.3,24.7,5,24,5c0,0,0,0,0,0c-0.7,0-1.4,0.3-1.9,0.8l-3.5,3.5l4.1,4.1c0.4-0.2,0.8-0.3,1.3-0.3c1.7,0,3,1.3,3,3c0,0.5-0.1,0.9-0.3,1.3l4,4c0.4-0.2,0.8-0.3,1.3-0.3c1.7,0,3,1.3,3,3s-1.3,3-3,3c-1.7,0-3-1.3-3-3c0-0.5,0.1-0.9,0.3-1.3l-4-4c-0.1,0-0.2,0.1-0.3,0.1v10.4c1.2,0.4,2,1.5,2,2.8c0,1.7-1.3,3-3,3s-3-1.3-3-3c0-1.3,0.8-2.4,2-2.8V18.8c-1.2-0.4-2-1.5-2-2.8c0-0.5,0.1-0.9,0.3-1.3l-4.1-4.1L5.8,22.1C5.3,22.6,5,23.3,5,24c0,0.7,0.3,1.4,0.8,1.9l16.3,16.3c0,0,0,0,0,0c0.5,0.5,1.2,0.8,1.9,0.8s1.4-0.3,1.9-0.8l16.3-16.3c0.5-0.5,0.8-1.2,0.8-1.9C43,23.3,42.7,22.6,42.2,22.1z"></path></svg>`;
-
-
-    tech_icons.forEach(icon => {
-        if (icon.classList.contains("figma")) {
-            icon.innerHTML = figma_svg_icon;
-        } else if (icon.classList.contains("github")) {
-            icon.innerHTML = github_svg_icon;
-        } else if (icon.classList.contains("python")) {
-            icon.innerHTML = python_svg_icon;
-        } else if (icon.classList.contains("html")) {
-            icon.innerHTML = html_svg_icon;
-        } else if (icon.classList.contains("css")) {
-            icon.innerHTML = css_svg_icon;
-        } else if (icon.classList.contains("js")) {
-            icon.innerHTML = js_svg_icon;
-        } else if (icon.classList.contains("django")) {
-            icon.innerHTML = django_svg_icon;
-        } else if (icon.classList.contains("git")) {
-            icon.innerHTML = git_svg_icon;
-        }
-    });
-
-
-
-
-    // proporção de altura e largura das imagens dos projetos
-    var project_images = document.querySelectorAll("#projects .container .project_line img");
-
-    window.onresize = () => {
-        project_images.forEach(image => {
-            image.style.height = image.offsetWidth/16*9 + "px";
-        });
-    };
-
-    project_images.forEach(image => {
-        image.style.height = image.offsetWidth/16*9 + "px";
-    });
-
-
-
-    // animação hover icons em contatos
-    var contact_icons = document.querySelectorAll("#contacts .line .container");
-
-    contact_icons.forEach(icon_hover => {
-        icon_hover.addEventListener("mouseover", () => {
-            icon_hover.classList.add("hover");
-
-            contact_icons.forEach(icon => {
-                if (!icon.classList.contains("hover")) {
-                    icon.style.opacity = ".8";
-                }
-            });
-        });
-
-        icon_hover.addEventListener("mouseout", () => {
-            contact_icons.forEach(icon => {
-                if (!icon.classList.contains("hover")) {
-                    icon.style.opacity = "1";
-                }
-            });
-
-            icon_hover.classList.remove("hover");
-        });
-    })
-
-
-    document.addEventListener("scroll", () => {
-            
-        document.querySelectorAll("section").forEach(section => {
-            if(window.scrollY + window.innerHeight >= section.offsetTop) {
-                document.querySelector(`nav ul li.${section.id}`).classList.add("shown");
-            } else {
-                document.querySelector(`nav ul li.${section.id}`).classList.remove("shown");
-            }
-        });
+    lang_switch.addEventListener("change", () => {
+        if (lang_switch.checked) {
+            // en
+            a_download_cv.href = "assets/cv/felipe_resume.pdf";
+        } else {
+            // ptbr
+            a_download_cv.href = "assets/cv/cv_dev_felipe.pdf";
+        };
     });
 });
